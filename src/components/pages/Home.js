53f01be10b8ba1e135036960c9a3787494272509
lucas de souza jacinto <lucasdesouzajacinto@gmail.com>
2023-12-21 20:48:59 -0300

    iniciando manipulação da pagina home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,15 @@
+import styles from "./Home.module.css";
+import savings from "../../img/savings.jpeg";
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <section className={styles.home_container}>
+      <h1>Bem Vindo ao Costs</h1>
+      <p>Comece a gerenciar seus projetos agora mesmo!</p>
+      <a href="/">Criar projeto</a>
+      <img src={savings} alt="sua mãe" />
+    </section>
+  );
 }
 
 export default Home;
