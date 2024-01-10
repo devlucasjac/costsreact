44f95936c41b26d5c85ec32f53e69a9aed969f60
lucas de souza jacinto <lucasdesouzajacinto@gmail.com>
2024-01-10 15:50:31 -0300

    estilização e estruturação da pagina de project

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,33 +3,69 @@ import styles from "./Project.module.css";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
+import Loading from "../layout/Loading";
+import Conteiner from "../layout/Conteiner";
+
 function Project() {
   const { id } = useParams();
 
   const [project, setProject] = useState();
+  const [showProjectForm, setShowProjectForm] = useState(false);
 
   useEffect(() => {
-    fetch(`http://127.0.0.1:8000/api/v1/projetos/${id}/`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        setProject(data);
-        console.log(data);
+    setTimeout(() => {
+      fetch(`http://127.0.0.1:8000/api/v1/projetos/${id}/`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
       })
-      .catch((err) => console.log(err));
+        .then((resp) => resp.json())
+        .then((data) => {
+          setProject(data);
+          console.log(data);
+        })
+        .catch((err) => console.log(err));
+    }, 300);
   }, [id]);
+
+  function toggleProjectForm() {
+    setShowProjectForm(!showProjectForm);
+  }
 
   return (
     <>
-      {project && (
-        <>
-          <div>{project.nome}</div>
-          <p>{project.orçamento}</p>
-        </>
+      {project ? (
+        <div className={styles.project_details}>
+          <Conteiner customClass="column">
+            <div className={styles.details_container}>
+              <h1>Projeto: {project.nome}</h1>
+              <button onClick={toggleProjectForm} className={styles.btn}>
+                {!showProjectForm ? "Editar projeto" : "Fechar"}
+              </button>
+              {!showProjectForm ? (
+                <div className={styles.project_info}>
+                  <p>
+                    <span>Categoria :</span> {project.categoria}
+                  </p>
+                  <p>
+                    <span>Orçamento : </span>
+                    {project.orçamento}
+                  </p>
+                  <p>
+                    <span>Total utilizado :</span> project.custo
+                  </p>
+                </div>
+              ) : (
+                <div className={styles.project_info}>
+                  <p>formulario</p>
+                </div>
+              )}
+            </div>
+          </Conteiner>
+        </div>
+      ) : (
+        <Loading />
       )}
     </>
   );
